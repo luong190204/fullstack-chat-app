ec2f37482dc929e2d58034bd9837c9ad582d3d87
Nguyen Dinh Luong <dinhluong19002004@gmail.com>
2025-07-15 11:02:55 +0700

    update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -21,8 +21,12 @@ const userSocketMap = {}; // {userId: socketId}
 io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
 
-    const userId = socket.handshake.query.userId
-    if (userId) userSocketMap[userId] = socket.id; // Gán userId vào socketId
+    const { userId } = socket.handshake.query;
+
+    // Kiểm tra userId tồn tại và hợp lệ
+    if (typeof userId === "string" && userId.trim() !== "") {
+      userSocketMap[userId] = socket.id; // Gán userId vào socketId
+    } 
 
     // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
