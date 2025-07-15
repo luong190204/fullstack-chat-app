56e564921503701ab54a1e878c121db68391e37d
Nguyen Dinh Luong <dinhluong19002004@gmail.com>
2025-07-15 10:31:57 +0700

    Project completed

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import { useChatStore } from "../store/useChatStore";
+
+import SideBar from "../components/Sidebar";
+import NochatSelected from "../components/NochatSelected";
+import ChatContainer from "../components/ChatContainer.jsx";
+
+const HomePage = () => {
+
+  const { selectedUser } = useChatStore();
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <SideBar />
+
+            {!selectedUser ? <NochatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomePage;
